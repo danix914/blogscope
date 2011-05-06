fe698648fc81219049227fa1339f17d1fd1a22a1
Det <danix914@gmail.com>
2011-05-06 15:45:20 +0800

    homework init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogscope::Application.config.secret_token = '6be67d5da6087ae5f266058b83831f611af245cebd23744f694f90158351be20d203e6bc071470663e622ae5ffb51716ef76efad15b06ca3e55d7f8ec67140c4'
