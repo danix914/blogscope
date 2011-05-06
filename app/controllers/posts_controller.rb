29593b728038eb01cabad583bd22913723371b0b
Det <danix914@gmail.com>
2011-05-06 19:15:54 +0800

    Handmake CRUD and view

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @posts }
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(params[:post])
+
+    #if @post.save
+      #redirect_to(@post, :notice => 'Posted!!')
+    #else
+      #render :action => "new"
+    #end
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to(@post, :notice => 'Posteeeeeeed!!') }
+        format.xml  { render :xml => @post, :status => :created, :location => @post }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(params[:post])
+      redirect_to(@post, :notice => 'Post updated!!<O>!!')
+    else
+      render :action => "edit"
+    end
+  end
+end
