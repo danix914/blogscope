f5d6d7bffab137b0fb26f47b556322254a2fa2bb
Det <danix914@gmail.com>
2011-05-08 18:35:57 +0800

    Study String.length and .split

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,3 +13,17 @@ class Post < ActiveRecord::Base
   scope :secret, where(:is_public => false)
   scope :is_publiced, lambda { |show| where(:is_public => show) }
 end
+
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer(4)      not null, primary key
+#  is_public  :boolean(1)
+#  name       :string(255)
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -6,3 +6,17 @@ class PostTest < ActiveSupport::TestCase
     assert true
   end
 end
+
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer(4)      not null, primary key
+#  is_public  :boolean(1)
+#  name       :string(255)
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
