098ca47a973ac982251dad2d1caf6df25a36597d
Det <danix914@gmail.com>
2011-07-28 17:14:02 +0800

    bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110509095033) do
+ActiveRecord::Schema.define(:version => 20110728090549) do
 
   create_table "posts", :force => true do |t|
     t.boolean  "is_public"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(:version => 20110509095033) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", :force => true do |t|
